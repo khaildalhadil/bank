9c63b1306369a855593b500759f2fec128d7046e
Khaild <khaild.alhadi2020@gmail.com>
2024-02-18 17:18:59 +0400

    first commit for all file ✔

---
+++ sinin/main.js
@@ -0,0 +1,12 @@
+// DOM
+
+const singIn = document.querySelector('.sing--in');
+const singUp = document.querySelector('.sing--up');
+
+
+// DOM 
+
+// event singup
+singUp.addEventListener('click', function () {
+    singIn.classList.add('hiddn')
+})
